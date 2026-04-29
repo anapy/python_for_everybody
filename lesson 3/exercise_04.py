287af5afd81921cc2ea6bcfa64bc2e328f1a3f78
anapy <anabelenfunes@gmail.com>
2026-04-29 11:24:50 +0200

    add exercises from lesson 3

---
+++ lesson 3/exercise_04.py
@@ -0,0 +1,13 @@
+#Exercise 4: Assume that we execute the following assignment statements
+
+width = 17
+height = 12.0
+
+a = width//2
+b = width/2.0
+c = height/3
+d = 1 + 2 * 5
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
---
+++ lesson 3/exercise_05.py
@@ -0,0 +1,4 @@
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+celcius = float(input('Give me the room temperature in Celsius: '))
+fahrenheit = celcius * 9 / 5 +32
+print(fahrenheit, 'ºF')
