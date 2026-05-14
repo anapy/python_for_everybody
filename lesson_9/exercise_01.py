ddab3495d920fba22a284e2b4a07bfcf8f344b2b
anapy <anabelenfunes@gmail.com>
2026-05-14 17:30:58 +0200

    adding exercise 1

---
+++ lesson_9/exercise_01.py
@@ -0,0 +1,19 @@
+#Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+
+#function that modifies the list
+def chop(array):
+    del array[0:(len(array) - 1)]
+    return None
+
+fruits = list(['banana', 'pear', 'orange'])
+chop(fruits)
+print(fruits)
+
+
+colors = list(['blue','purple','red', 'yellow'])
+#function that keeps the list and return a new list
+def middle(array):
+    return array[1:len(array) - 1]
+
+print(middle(colors))
+print(colors)
