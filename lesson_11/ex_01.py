08ebc87cf6e7929833b49c340d73ddcf2bbb4b3f
anapy <anabelenfunes@gmail.com>
2026-06-04 20:17:25 +0200

    adding some tuples exercises

---
+++ lesson_11/ex_01.py
@@ -0,0 +1,29 @@
+'''Exercise 1: Revise a previous program as follows: Read and parse the “From” lines and pull out the addresses from the line. Count the number of messages from each person using a dictionary.
+
+After all the data has been read, print the person with the most commits by creating a list of (count, email) tuples from the dictionary. Then sort the list in reverse order and print out the person who has the most commits.
+
+Sample Line:
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+Enter a file name: mbox-short.txt
+cwen@iupui.edu 5
+
+Enter a file name: mbox.txt
+zqian@umich.edu 195'''
+
+file = open('mbox-short.txt')
+
+emails = dict()
+for line in file:
+    line = line.rstrip()
+    words = line.split(' ')
+    if words[0] != 'From' or len(words) < 3: continue
+    emails[words[1]] = emails.get(words[1], 0) + 1
+
+emailsTuple = list()
+for key, val in emails.items():
+    emailsTuple.append((val, key))
+    emailsTuple.sort(reverse=True)
+
+print(emailsTuple[0])
+
